ad785755ee352a3f22de7cf03142129e4b18cc0d
Arfin Usmaan <arfinusmaan008@gmail.com>
2025-06-30 12:43:44 +0530

    Changed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,39 +1,39 @@
 // src/assets/index.js
 
-export const Django = "/logo/tech/Django.svg";
-export const Python = "/logo/tech/Python.svg";
-export const html = "/logo/tech/html.png";
-export const css = "/logo/tech/css.png";
-export const javascript = "/logo/tech/javascript.png";
-export const mysql = "/logo/tech/mysql.svg";
-export const git = "/logo/tech/git.png";
-export const reactjs = "/logo/tech/reactjs.png";
-export const redux = "/logo/tech/redux.png";
-export const tailwind = "/logo/tech/tailwind.png";
-export const threejs = "/logo/tech/threejs.svg";
-export const gsap = "/logo/tech/gsap.png";
-export const appwrite = "/logo/tech/appwrite.svg";
+export const Django = "/assets/tech/Django.svg";
+export const Python = "/assets/tech/Python.svg";
+export const html = "/assets/tech/html.png";
+export const css = "/assets/tech/css.png";
+export const javascript = "/assets/tech/javascript.png";
+export const mysql = "/assets/tech/mysql.svg";
+export const git = "/assets/tech/git.png";
+export const reactjs = "/assets/tech/reactjs.png";
+export const redux = "/assets/tech/redux.png";
+export const tailwind = "/assets/tech/tailwind.png";
+export const threejs = "/assets/tech/threejs.svg";
+export const gsap = "/assets/tech/gsap.png";
+export const appwrite = "/assets/tech/appwrite.svg";
 
 // General
-export const logo = "/logo/logo.svg";
-export const fullstack = "/logo/fullstack.png";
-export const threed = "/logo/threed.png";
-export const react = "/logo/react.png";
-export const frontend = "/logo/frontend.png";
-export const github = "/logo/github.png";
-export const live = "/logo/live.png";
-export const menu = "/logo/menu.svg";
-export const close = "/logo/close.svg";
+export const logo = "/assets/logo.svg";
+export const fullstack = "/assets/fullstack.png";
+export const threed = "/assets/threed.png";
+export const react = "/assets/react.png";
+export const frontend = "/assets/frontend.png";
+export const github = "/assets/github.png";
+export const live = "/assets/live.png";
+export const menu = "/assets/menu.svg";
+export const close = "/assets/close.svg";
 
 // Company
-export const inmakes = "/logo/company/inmakes.png";
-export const unified = "/logo/company/unified.png";
+export const inmakes = "/assets/company/inmakes.png";
+export const unified = "/assets/company/unified.png";
 
 // Projects
-export const nike = "/logo/nike.png";
-export const qr = "/logo/qr.png";
-export const moodflix = "/logo/moodflix.png";
-export const lyriks = "/logo/lyriks.png";
-export const expense = "/logo/expense.png";
-export const recipe = "/logo/recipe.png";
-export const ecom = "/logo/ecom.png";
+export const nike = "/assets/nike.png";
+export const qr = "/assets/qr.png";
+export const moodflix = "/assets/moodflix.png";
+export const lyriks = "/assets/lyriks.png";
+export const expense = "/assets/expense.png";
+export const recipe = "/assets/recipe.png";
+export const ecom = "/assets/ecom.png";
